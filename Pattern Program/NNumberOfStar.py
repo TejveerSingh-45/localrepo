8dadf1b812a65f0baa1d42fc7e0d0cd81e912bef
TejveerSingh-45 <ts6145529@gmail.com>
2025-11-19 22:16:52 +0530

    Pattern program

---
+++ Pattern Program/NNumberOfStar.py
@@ -0,0 +1,3 @@
+n=int(input("enter number"))
+for i in range(1,5):
+    print(i,end="")
---
+++ Pattern Program/TriangleStar.py
@@ -0,0 +1,10 @@
+# Method 1:
+n=int(input("enter any number"))
+for i in range(1,n+1):
+    for j in range(i):
+        print("*",end="")
+    print()
+# Method 2:
+# n=int(input("enter any number"))
+# for i in range(1,5):
+#     print("*" *i)
